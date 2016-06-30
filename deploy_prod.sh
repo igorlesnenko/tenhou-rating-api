8c022ad0c72750bb363645d7ee1eadb8e31a758e
Igor <igor.lesnenko@gmail.com>
2016-06-30 18:33:46 +0400

    Fix deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker login -p $DOCKER_PASSWORD -u $DOCKER_ID_USER
+docker login -e $DOCKER_EMAIL -p $DOCKER_PASSWORD -u $DOCKER_ID_USER
 docker tag tenhou:api $DOCKER_ID_USER/tenhou:api
 docker push $DOCKER_ID_USER/tenhou:api
